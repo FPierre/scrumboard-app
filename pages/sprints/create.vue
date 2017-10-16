300a666c25870fc3407a2c6f5cb59d7553ce7a5b
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-10-16 18:04:36 +0200

    Creation du squelette de la page d'ajout d'un sprint

--- pages/sprints/create.vue
+++ pages/sprints/create.vue
@@ -1,5 +1,43 @@
 <template>
   <div class='sprints-create-page'>
-    Create
+    <h1 class='title'>New sprint #{{ id }}</h1>
+
+    <div class='field'>
+      <label class='label'>How many developers participate?</label>
+      <div class='control'>
+        <input class='input' type='text' placeholder='3, 4, 5, ...' v-model='developers'>
+      </div>
+    </div>
+
+    <div class='field'>
+      <label class='label'>How many effort points are estimated?</label>
+      <div class='control'>
+        <input class='input' type='text' placeholder='40, 75.5, 100, ...' v-model='points'>
+      </div>
+    </div>
+
+    <div class='field'>
+      <label class='label'>How many days the sprint will covers?</label>
+      <div class='control'>
+        <input class='input' type='text' placeholder='5, 10, 20, ...' v-model='days'>
+      </div>
+    </div>
+
+    <p>The new sprint (#{{ id }}) is composed of {{ developers }} developers who must complete {{ points }} points on {{ days }} days.</p>
+
+    <button>Create</button>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      id: 1,
+      developers: null,
+      points: null,
+      days: null
+    }
+  }
+}
+</script>
