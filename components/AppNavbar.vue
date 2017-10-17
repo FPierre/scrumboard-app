edf47a4f668646b9c909e9bdedc042c543d9792e
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-10-17 17:47:12 +0200

    Component basique de navigation

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -1,19 +1,19 @@
 <template>
   <div class='app-navbar-component'>
-    <div class='navbar-menu'>
+    <nav class='navbar is-primary' role='navigation' aria-label='main navigation'>
       <div class='navbar-start'>
-        <nuxt-link :to='{ name: "" }' class='navbar-item'>
+        <nuxt-link :to='{ name: "Home" }' class='navbar-item'>
           <span class='icon is-medium has-text-white'>
             <i class='fa fa-long-arrow-left'></i>
           </span>
         </nuxt-link>
       </div>
 
-      <div class='navbar-end'>
+      <!-- <div class='navbar-end'>
         <div class='navbar-item'>
           Create
         </div>
-      </div>
-    </div>
+      </div> -->
+    </nav>
   </div>
 </template>
