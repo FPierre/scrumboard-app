977f256a0172422cf9791fcad8a772fe40365acc
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-10-17 09:55:17 +0200

    WIP effort accompli la veille

--- pages/sprints/_id.vue
+++ pages/sprints/_id.vue
@@ -1,5 +1,35 @@
 <template>
   <div class='sprint-page'>
-    ID
+    <h1 class='title'>Sprint #{{ id }}</h1>
+    <h2 class='subtitle'>Day {{ day }}/{{ days }}</h2>
+
+    <div class='field'>
+      <label class='label'>How many effort point done yesterday?</label>
+      <div class='control'>
+        <input class='input' type='text' placeholder='3, 4, 5, ...' v-model='pointsDone'>
+      </div>
+    </div>
+
+    <button>Save</button>
   </div>
-</template
+</template>
+
+<script>
+// import axios from 'axios'
+
+export default {
+  async asyncData ({ params }) {
+    // let { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+
+    return {
+      day: 2,
+      days: 5
+    }
+  },
+  data () {
+    return {
+      pointsDone: null
+    }
+  }
+}
+</script>
