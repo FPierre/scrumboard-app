14725e47bc34c08ff739c7df5e0a3214e2c477ec
FPierre <pflauder@gmail.com>
2017-10-22 14:23:19 +0200

    Fix bug hover navbar

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='app-navbar-component'>
     <nav class='navbar is-primary' role='navigation' aria-label='main navigation'>
-      <div class='navbar-start'>
+      <div class='navbar-brand'>
         <nuxt-link :to='{ name: "index" }' class='navbar-item'>
           <span class='icon is-medium has-text-white'>
             <i class='fa fa-long-arrow-left'></i>
