9cb26cc32c07e882b5cb35d479c31bb4952939aa
FPierre <pflauder@gmail.com>
2017-10-25 11:59:43 +0200

    Deplacement du component steps

---
+++ components/sprints/create/Steps.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class='steps-component'>
+    <div class='steps is-flex'>
+      <div class='step' v-for='(step, index) in size' :class='{ active: current > index }'></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['size', 'current']
+}
+</script>
+
+<style lang='scss' scoped>
+@import '~assets/stylesheets/bulma-override';
+
+.steps-component {
+  height: 100%;
+}
+
+.steps {
+  align-items: center;
+  flex-direction: column;
+  margin-top: 1.7rem;
+}
+
+.step {
+  background-color: $grey-lighter;
+  border-radius: 9px;
+  height: 9px;
+  margin: .2rem;
+  width: 9px;
+
+  &.active {
+    background-color: $grey-darker;
+  }
+}
+</style>
